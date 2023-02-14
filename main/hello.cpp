876edf3e6dfd2dc467ed94cc924dc52bf5fb7bdb
Varun ShankarHoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2023-02-14 14:45:32 +0530

    made changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, Varun SH - PES1UG20CS490, have successfully built and run\n";
+  cout << "I, Varun SH - PES1UG20CS490 - quickly swiftly slow, have successfully built and run\n";
   return 0;
 }
